1a70201b573658e049351a1e921bb3c9c3f14bb6
iyabc <juliennepanes@gmail.com>
2024-04-03 22:20:09 +0800

    refactor(env): changed env implementation in supabase.ts

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,9 +1,10 @@
 import 'react-native-url-polyfill/auto';
+import { SUPABASE_URL, SUPABASE_KEY } from '@env';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL || '';
-const supabaseAnonKey = process.env.SUPABASE_KEY || '';
+const supabaseUrl = SUPABASE_URL || '';
+const supabaseAnonKey = SUPABASE_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
